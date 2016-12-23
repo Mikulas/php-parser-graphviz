50876ecc1dad67bbccab29ce43a74dcc1bc75fc9
Mikulas <rullaf@gmail.com>
2016-12-23 19:06:21 +0100

    rewrite dumper

--- src/Mikulas/PhpParser/GraphvizDumper.php
+++ src/Mikulas/PhpParser/GraphvizDumper.php
@@ -63,36 +63,21 @@ class GraphvizDumper
 			DotNode::OPT_LABEL => $node->getType(),
 		]);
 
+		$dot->setOption(DotNode::OPT_SUBLABEL, $this->getSublabel($node));
+
 		foreach ($node->getSubNodeNames() as $key) {
 			$value = $node->$key;
 
-			$sublabel = NULL;
-			if (false === $value) {
-				$sublabel = 'false';
-			} elseif (true === $value) {
-				$sublabel = 'true';
-			} elseif (is_scalar($value)) {
-				if ('flags' === $key || 'newModifier' === $key) {
-					$sublabel = $this->dumpFlags($value);
-				} else if ('type' === $key && $node instanceof Include_) {
-					$sublabel = $this->dumpIncludeType($value);
-				} else if ('type' === $key
-					&& ($node instanceof Use_ || $node instanceof UseUse || $node instanceof GroupUse)) {
-					$sublabel = $this->dumpUseType($value);
-				} else {
-					$sublabel = $value;
-				}
-			} elseif ($node instanceof Node\Name) {
-				$sublabel = $this->getSublabel($node);
+			if ($value instanceof Node) {
+				$dot->addChild($this->convertSingle($value));
 
-			} elseif ($value instanceof Node || is_array($value)) {
-				foreach ($this->convert($value) as $child) {
-					$dot->addChild($child);
+			} elseif (is_array($value)) {
+				foreach ($value as $v) {
+					if ($v instanceof Node) {
+						$dot->addChild($this->convertSingle($v));
+					}
 				}
-
-				$sublabel = $this->getSublabel($node);
 			}
-			$dot->setOption(DotNode::OPT_SUBLABEL, $sublabel);
 		}
 
 		return $dot;
@@ -108,82 +93,13 @@ class GraphvizDumper
 			return $node->name;
 
 		} elseif ($node instanceof Node\Param) {
-			return $node->type . ' ' . $node->name;
+			return trim($node->type . ' ' . $node->name);
 
+		} elseif ($node instanceof Node\Expr\Variable) {
+			return $node->name;
 		}
+
 		return NULL;
 	}
 
-
-	protected function dumpFlags($flags) {
-		$strs = [];
-		if ($flags & Class_::MODIFIER_PUBLIC) {
-			$strs[] = 'MODIFIER_PUBLIC';
-		}
-		if ($flags & Class_::MODIFIER_PROTECTED) {
-			$strs[] = 'MODIFIER_PROTECTED';
-		}
-		if ($flags & Class_::MODIFIER_PRIVATE) {
-			$strs[] = 'MODIFIER_PRIVATE';
-		}
-		if ($flags & Class_::MODIFIER_ABSTRACT) {
-			$strs[] = 'MODIFIER_ABSTRACT';
-		}
-		if ($flags & Class_::MODIFIER_STATIC) {
-			$strs[] = 'MODIFIER_STATIC';
-		}
-		if ($flags & Class_::MODIFIER_FINAL) {
-			$strs[] = 'MODIFIER_FINAL';
-		}
-
-		if ($strs) {
-			return implode(' | ', $strs) . ' (' . $flags . ')';
-		} else {
-			return $flags;
-		}
-	}
-
-	protected function dumpIncludeType($type) {
-		$map = [
-			Include_::TYPE_INCLUDE  => 'TYPE_INCLUDE',
-			Include_::TYPE_INCLUDE_ONCE => 'TYPE_INCLUDE_ONCE',
-			Include_::TYPE_REQUIRE  => 'TYPE_REQUIRE',
-			Include_::TYPE_REQUIRE_ONCE => 'TYPE_REQURE_ONCE',
-		];
-
-		if (!isset($map[$type])) {
-			return $type;
-		}
-		return $map[$type] . ' (' . $type . ')';
-	}
-
-	protected function dumpUseType($type) {
-		$map = [
-			Use_::TYPE_UNKNOWN  => 'TYPE_UNKNOWN',
-			Use_::TYPE_NORMAL   => 'TYPE_NORMAL',
-			Use_::TYPE_FUNCTION => 'TYPE_FUNCTION',
-			Use_::TYPE_CONSTANT => 'TYPE_CONSTANT',
-		];
-
-		if (!isset($map[$type])) {
-			return $type;
-		}
-		return $map[$type] . ' (' . $type . ')';
-	}
-
-	// Copied from Error class
-
-
-	private function toColumn($code, $pos) {
-		if ($pos > strlen($code)) {
-			throw new \RuntimeException('Invalid position information');
-		}
-
-		$lineStartPos = strrpos($code, "\n", $pos - strlen($code));
-		if (false === $lineStartPos) {
-			$lineStartPos = -1;
-		}
-
-		return $pos - $lineStartPos;
-	}
 }
